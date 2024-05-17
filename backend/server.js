aa74afa88ef180116a6f1381a8043145a7a76cbc
Dr-AddictStein <dr.addictstein@gmail.com>
2024-05-17 21:02:23 +0600

    routes-done

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import workOutRoutes from './routes/workouts.js'
+
 dotenv.config();
 
 // creates express app
 const app = express();
 
 // use of middlewars
+app.use(express.json())
+
 app.use((req,res,next)=>{
     console.log(req.path,req.method)
     next();
 })
 
 
-
-
-app.get('/',(req,res)=>{
-    res.json({
-        mssg:"Backend OK"
-    })
-})
+app.use('/api/workOuts',workOutRoutes);
 
 
 // listen for request
