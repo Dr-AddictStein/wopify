1fc4189332a80d5cae0a6a1f044badb5c809a1aa
Dr-AddictStein <dr.addictstein@gmail.com>
2024-05-17 21:15:25 +0600

    DB connected

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose, { connect } from 'mongoose';
 
 import workOutRoutes from './routes/workouts.js'
 
@@ -20,7 +21,16 @@ app.use((req,res,next)=>{
 app.use('/api/workOuts',workOutRoutes);
 
 
-// listen for request
-app.listen(process.env.PORT,()=>{
-    console.log(`Listening on PORT ${process.env.PORT}`)
-});
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        // listen for request
+        console.log("Successfully Connected to DB")
+        app.listen(process.env.PORT,()=>{
+            console.log(`Listening on PORT ${process.env.PORT}`)
+        });
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+
