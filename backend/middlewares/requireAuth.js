1624cbb9aeac080aa3314c503aee44042ba0bc36
Dr-AddictStein <dr.addictstein@gmail.com>
2024-05-22 06:22:38 +0600

    added-auth-token

---
+++ backend/middlewares/requireAuth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+import user from '../models/UserModel.js';
+const requireAuth = async(req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ error: "Auth Token Required.!." });
+  }
+
+  const token = authorization.split(' ')[1];
+
+  try {
+    const {_id}=jwt.verify(token,process.env.SECRET);
+
+    req.user=await user.findOne({_id}).select('_id');
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({error:"Request is not authorized.!."});
+  }
+};
+
+export default requireAuth;
--- backend/routes/workoutRoutes.js
+++ backend/routes/workoutRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import workoutModel from '../models/WorkoutModel.js'
 import { createWorkout, deleteWorkout, getAllWorkouts, getSingleWorkout, updateWorkout } from '../controllers/workoutControllers.js';
+import requireAuth from '../middlewares/requireAuth.js';
+
 
 const router = express.Router();
 
-
+router.use(requireAuth);
 
 router.get('/',getAllWorkouts)
 router.get('/:id',getSingleWorkout)
